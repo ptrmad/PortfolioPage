fe40436e7eaf6587def6ff38def361131f6392e4
Piotr <piotr.m.drozdzal@gmail.com>
2024-06-12 14:08:39 +0200

    Input message changed to textarea

--- src/componets/ContactForm/ContactForm.tsx
+++ src/componets/ContactForm/ContactForm.tsx
@@ -1,16 +1,22 @@
 import styles from "./ContactForm.module.css";
 
 export function ContactForm() {
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+    }
+  };
+
   return (
     <form action="send" className={styles.form}>
       <input className={styles.formInput} type="text" placeholder="Name" />
       <input className={styles.formInput} type="mail" placeholder="Email" />
       <input className={styles.formInput} type="text" placeholder="Company" />
-      <input
+      <textarea
         className={styles.inputMessage}
-        type="text"
         placeholder="Your message..."
-      />
+        onKeyPress={handleKeyPress}
+      ></textarea>
       <button className={styles.button}>Send</button>
     </form>
   );
