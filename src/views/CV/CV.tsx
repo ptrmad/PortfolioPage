594f67f3231b9c0fa193e64dd12dce7b9990b10b
Piotr <piotr.m.drozdzal@gmail.com>
2024-12-01 15:18:23 +0100

    CV updated

--- src/views/CV/CV.tsx
+++ src/views/CV/CV.tsx
@@ -8,7 +8,7 @@ export function CV() {
     <div className={styles.cv}>
       <SlideInWrapper>
         <H2>CV</H2>
-        <iframe src="https://drive.google.com/file/d/1GLiXyO75n8GaR7ReRlJ4i3odatr9RY87/preview"></iframe>
+        <iframe src="https://drive.google.com/file/d/1djxp5Uz1pozbE2q8jO_lhjB9C-jOzLq0/view?usp=sharing"></iframe>
       </SlideInWrapper>
       {/* <H2>Certificates</H2>
       <Certificate />
